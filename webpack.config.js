08b2f674618ec8987660d4ce885616be9d0b902a
harryxu <harryzhxu@gmail.com>
2017-08-10 12:02:30 +0800

    basic webpack config

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const webpack = require('webpack')
+
+const __ENV__ = process.env.NODE_ENV || 'development'
+const __DEV__ = __ENV__ === 'development'
+const __TEST__ = __ENV__ === 'test'
+const __PROD__ = __ENV__ === 'production'
+
+// config
+const publicPath = path.resolve(__dirname, 'public')
+
+const config = {
+  entry: {
+    index: './src/index.js'
+  },
+  devtool: __DEV__ ? 'source-map' : false,
+  output: {
+    filename: 'js/[name].bundle.js',
+    path: publicPath
+  },
+  module: {
+    rules: []
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(__ENV__)
+    })
+  ]
+}
+
+if (__PROD__) {
+  config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false,
+      drop_console: true
+    }
+  }))
+}
+
+config.module.rules.push({
+  test: /\.jsx?$/,
+  use: 'babel-loader',
+  exclude: [/node_modules/]
+})
+
+module.exports = config
